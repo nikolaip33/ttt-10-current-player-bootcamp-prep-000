6847a39315a45e20495cbe00c0a3be85ec158fa2
Nikolai Pawlowski <nikolaip@gmail.com>
2017-09-20 20:27:24 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,19 @@
 def turn_count(board)
-  turn = 1
+  turn = 0
   board.each do |space|
-    if turn != " "
+    if space != " "
       turn += 1
     end
   end
   turn
+end
+
+def current_player(board)
+  turn = turn_count(board)
+  if turn%2 == 0
+    current_player = "0"
+  else
+    current_player = "X"
+  end
+  current_player
 end
